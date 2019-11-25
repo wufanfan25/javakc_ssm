c652291b75b66fef0c9af2eed51b3435493db7f1
997758131 <13463526182@163.com>
2019-11-25 10:48:03 +0800

    物资-实体类1更改

--- src/main/java/com/zhg/javakc/modules/material/controller/MaterialController.java
+++ src/main/java/com/zhg/javakc/modules/material/controller/MaterialController.java
@@ -1,4 +1,29 @@
 package com.zhg.javakc.modules.material.controller;
 
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.modules.material.entity.MaterialEntity;
+import com.zhg.javakc.modules.material.service.MaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/material")
 public class MaterialController {
+
+    @Autowired
+    MaterialService materialService;
+
+    @RequestMapping("/queryList")
+    public ModelAndView queryTest(MaterialEntity materialEntity, HttpServletRequest request, HttpServletResponse response){
+        ModelAndView modelAndView = new ModelAndView("material/list");
+        Page<MaterialEntity> page = materialService.queryTest(materialEntity, new Page<MaterialEntity>(request, response));
+        modelAndView.addObject(page);
+        return modelAndView;
+    }
+
 }
--- src/main/java/com/zhg/javakc/modules/material/service/MaterialService.java
+++ src/main/java/com/zhg/javakc/modules/material/service/MaterialService.java
@@ -1,4 +1,29 @@
 package com.zhg.javakc.modules.material.service;
 
-public class MaterialService {
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.base.service.BaseService;
+import com.zhg.javakc.modules.material.dao.MaterialDao;
+import com.zhg.javakc.modules.material.entity.MaterialEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MaterialService extends BaseService<MaterialDao, MaterialEntity> {
+
+    @Autowired
+    MaterialDao materialDao;
+
+    public Page<MaterialEntity> queryTest(MaterialEntity materialEntity,Page<MaterialEntity> page){
+        //设置分页参数，才会被MyBatis分页插件所识别，拦截sql，并加入分页sql语句
+        materialEntity.setPage(page);
+        //根据分页与查询条件 查询
+        List<MaterialEntity> materialList = materialDao.findList(materialEntity);
+        return page.setList(materialList);
+    }
+
+
+
+
 }
