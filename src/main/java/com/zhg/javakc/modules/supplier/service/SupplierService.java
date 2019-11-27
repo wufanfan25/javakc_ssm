0f272406ccdce082296513a4f9ac09e6c9a327a5
zdy_global <s21210_global@126.com>
2019-11-27 19:35:23 +0800

    供应商编码service类

--- src/main/java/com/zhg/javakc/modules/supplier/service/SupplierService.java
+++ src/main/java/com/zhg/javakc/modules/supplier/service/SupplierService.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.processing.SupportedOptions;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SupplierService extends BaseService<SupplierDao, SupplierEntity>{
@@ -20,6 +22,24 @@ public class SupplierService extends BaseService<SupplierDao, SupplierEntity>{
     //根据分页与查询条件 查询
     List<SupplierEntity> listSupplier   =supplierDao.findList(supplierEntity);
     return page.setList(listSupplier);
+  }
+
+  //在新增前获取数据库的序列值 通过逻辑设置转变为字符串编码
+  public String findSupplierCode(){
+    //编码格式 7000001  7 + 000001
+    //      7(固定字符) + 000001(序列)
+    //获取序列
+    Integer supCode=dao.findSupplierCode();
+    //变为字符串,并获取字符串长度
+    String str1=supCode.toString();
+    int length = str1.length();
+    //保存0的个数
+    String str2="";
+    for (int i=0;i<6-length;i++){
+        str2 += "0";
+    }
+    String supplierCode = '7' + str2 + str1;
+    return supplierCode;
   }
 
 
